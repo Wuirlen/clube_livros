da6d9f7e742d8ee4cceff13877916022b1ab3a56
Tutiplast <portariatutiplast@gmail.com>
2021-05-28 15:38:54 -0400

    Projeto

---
+++ README.md
@@ -0,0 +1,33 @@
+# Lara_Biblioteca
+ Sistema de biblioteca com o laravel 7.
+ 
+# Introdução
+Esboço de um simples sistema de biblioteca.
+
+# Ações disponiveis
+CRUD de usuários, alunos, categorias e livros.
+
+CRUD de consultas dos usuários, alunos, livros, categorias e livros emprestados.
+
+Usuário admin usando o seeder.
+
+##### email: admin@admin.com
+##### senha: admin
+
+# ScreenShots
+
+![login](https://github.com/DCO20/lara_biblioteca/blob/master/login.png)
+
+![dashboard](https://github.com/DCO20/lara_biblioteca/blob/master/dashboard.png)
+
+![users](https://github.com/DCO20/lara_biblioteca/blob/master/users.png)
+
+![studants1](https://github.com/DCO20/lara_biblioteca/blob/master/studants01.png)
+
+![studants2](https://github.com/DCO20/lara_biblioteca/blob/master/studants02.png)
+
+![libros1](https://github.com/DCO20/lara_biblioteca/blob/master/libros1.png)
+
+![libros2](https://github.com/DCO20/lara_biblioteca/blob/master/libros2.png)
+
+![categories](https://github.com/DCO20/lara_biblioteca/blob/master/categories.png)
---
+++ app/User.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'document','date_of_birth', 'email', 'avatar', 'genre', 'phone', 'password', 
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function setDateOfBirthAttribute($value)
+    {
+        $this->attributes['date_of_birth'] = $this->convertStringToDate($value);
+    }
+
+    public function getDateOfBirthAttribute($value)
+    {
+        return date('d/m/Y', strtotime($value));
+    }
+
+    public function setDocumentAttribute($value)
+    {
+        $this->attributes['document'] = $this->clearField($value);
+    }
+
+    public function getDocumentAttribute($value)
+    {
+        return substr($value, 0, 3) . '.' . substr($value, 3, 3) . '.' . substr($value, 6, 3) . '-' . substr($value, 9, 2);
+    }
+
+    public function setPhoneAttribute($value)
+    {
+        $this->attributes['phone'] = $this->clearField($value);
+    }
+
+    private function convertStringToDate(?string $param)
+    {
+        if(empty($param)){
+            return null;
+        }
+
+        list($day, $month, $year) = explode('/', $param);
+        return (new \DateTime($year . '-' . $month . '-' . $day))->format('Y-m-d');
+    }
+
+    private function clearField(?string $param)
+    {
+        if(empty($param)){
+            return '';
+        }
+
+        return str_replace(['.', '-', '/', '(', ')', ' '], '', $param);
+    }
+}
